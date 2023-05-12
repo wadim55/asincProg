d43019b6feb814d431be7ab55a8ff04ecb926ca3
wadim55 <korkin_wadim@mail.ru>
2023-05-12 18:07:58 +0600

    асинхронные методы

--- Assets/manager.cs
+++ Assets/manager.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class manager : MonoBehaviour
@@ -9,10 +10,11 @@ public class manager : MonoBehaviour
     [SerializeField] private float speedRotate;
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A)) StartCoroutine("LifeRoutine");
+       // if (Input.GetKeyDown(KeyCode.A)) StartCoroutine("LifeRoutine");
+       if (Input.GetKeyDown(KeyCode.A)) LifeAsync();
     }
 
-    private IEnumerator LifeRoutine()
+  /*  private IEnumerator LifeRoutine()
     {
         for (var i = 0; i < CubeList.Length; i++)
         {
@@ -29,5 +31,26 @@ public class manager : MonoBehaviour
             o.Rotate(Vector3.up, speedRotate);
             yield return null;
         }
+    }*/
+
+    private async void LifeAsync()
+    {
+        for (var i = 0; i < CubeList.Length; i++)
+        {
+           await  RotateAsync(CubeList[i], 1);
+        }
     }
+
+    private async Task RotateAsync(Transform o, float duration)
+    {
+        float timer = 0;
+        while (timer < 1)
+        {
+            timer = Math.Min(timer + Time.deltaTime / duration, 1);
+            o.Rotate(Vector3.up, speedRotate);
+
+            await Task.Yield();
+        }
+    }
+    
 }
